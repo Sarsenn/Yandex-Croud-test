73da9449f72a770efc21a0240c41b6a7a6221fa9
Sarsen <blazer.fei@gmail.com>
2024-08-01 11:37:10 +0500

    Littel Fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 //Исходные данные по слайдеру(const)
 const sliderConent = document.querySelectorAll('.slider_content'),
 	  sliderLine = document.querySelector('.slider_line'),
-	  slider_dots = document.querySelector('.slider_dots'),
+	  sliderDots = document.querySelectorAll('.slider_dots'),
 	  sliderNextBtn = document.querySelector('.slider_btn-next'),
 	  sliderPrevBtn = document.querySelector('.slider_btn-prev');
 
@@ -30,17 +30,24 @@ showSlide()
 //Перелеситывает слайдер вперед
 function nextSlide () {
 	sliderCount ++;
+	console.log(sliderCount)
 	if(sliderCount >= sliderConent.length) sliderCount = 0;
+	chekSlider()
 	rollSlide()
+	thisSlide(sliderCount)
 }
 
 
 // Перелистывает слайд назада 
 
 function prevSlide () {
+
 	sliderCount--;
-	if(sliderCount < 0) sliderCount = sliderConent.length - 1;
+
+	if(sliderCount < 0 && sliderConent !== 0) sliderCount = sliderConent.length - 1;
+	chekSlider()
 	rollSlide()
+	thisSlide(sliderCount)
 }
 
 
@@ -49,3 +56,28 @@ function prevSlide () {
 function rollSlide () {
 	sliderLine.style.transform =  `translateX(${-sliderCount * sliderWidth}px)`;
 }
+
+// Указывет какой слайд по счету активный 
+function thisSlide (index) {
+	sliderDots.forEach(item => item.classList.remove('active_dots'));
+	sliderDots[index].classList.add('active_dots')
+}
+
+sliderDots.forEach((dot, index) => {
+	dot.addEventListener('click', () => {
+		sliderCount = index;
+		rollSlide();
+		thisSlide(sliderCount);
+		chekSlider()
+	})
+}) 
+
+function chekSlider () {
+	if (sliderCount > 0 || sliderCount == 5) {
+		sliderPrevBtn.removeAttribute('disabled')
+	} else {
+		sliderPrevBtn.setAttribute('disabled', 'disabled')
+	}
+}
+
+chekSlider();
